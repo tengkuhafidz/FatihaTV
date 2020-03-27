09081fd36c8fbf5169daf2e1e1dfaeadf696b1d8
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-03-28 00:47:28 +0800

    filter to only show upcoming live sessions

--- src/components/Live/live-sessions.tsx
+++ src/components/Live/live-sessions.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import moment from 'moment'
 
 import SingleLiveSession from './single-live-session'
-import mergedPlaylistData from '../../data/merged-playlist-video-data.json'
+import LiveSessionsData from '../../data/live-sessions-data.json'
 
 const LiveSessions = () => {
+    const isUpcoming = (dateWithoutYear: string) => {
+        const currentYear = moment().get('year')
+        const dateWithYear = `${dateWithoutYear} ${currentYear}`
+        return moment(dateWithYear).isAfter()
+    }
     const renderLiveSessions = () => {
-        return mergedPlaylistData.map(() => <SingleLiveSession />)
+        const upcomingLiveSessionsData = LiveSessionsData.filter((liveSession) => isUpcoming(liveSession.Date))
+        return upcomingLiveSessionsData.map((data) => <SingleLiveSession data={data} />)
     }
     return (
         <div className="container mx-auto px-8 pt-16 pb-32" id="liveSessions">

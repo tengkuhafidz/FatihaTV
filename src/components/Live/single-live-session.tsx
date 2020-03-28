237655cba7f917185e122ff5e47803e09d6c2c18
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-03-28 13:09:08 +0800

    add display day

--- src/components/Live/single-live-session.tsx
+++ src/components/Live/single-live-session.tsx
@@ -4,27 +4,46 @@ import moment from 'moment'
 const SingleLiveSession = ({data}) => {
     const { Time, Mosque, Title, Speaker, Link } = data
 
-    const getCalendarLink = () => {
-        const details = `By+${Speaker},+Masjid+${Mosque}`
+    const getDateTime = () => {
         const currentYear = moment().get('year')
         const dateTime = `${data.Date} ${currentYear} ${data.Time}`
-        const formattedDateTime = moment(dateTime).format('YYYYMMDDTHHmmss')
-        return `https://calendar.google.com/calendar/r/eventedit?text=${Title}&dates=${formattedDateTime}/${formattedDateTime}&details=${details}`
+        return moment(dateTime).format('YYYYMMDDTHHmmss')
+    }
+
+    const getCalendarLink = () => {
+        const details = `By+${Speaker},+Masjid+${Mosque}`
+        const dateTime = getDateTime
+        return `https://calendar.google.com/calendar/r/eventedit?text=${Title}&dates=${dateTime}/${dateTime}&details=${details}`
     }
 
     const getFormattedDate = () => {
         const numberOnlyPattern = /\d+/g;
+        const date = data.Date
+        const displayDate = moment(getDateTime()).calendar(null, {
+            sameDay: '[Today]',
+            nextDay: '[Tomorrow]',
+            nextWeek: 'dddd',
+            lastDay: '[Yesterday]',
+            lastWeek: '[Last] dddd',
+            sameElse: 'DD/MM/YYYY'
+        });
+
         return {
-            numericDay: data.Date.match(numberOnlyPattern),
-            month: data.Date.replace(numberOnlyPattern, '')
+            displayDay: displayDate,
+            numericDay: date.match(numberOnlyPattern),
+            month: date.replace(numberOnlyPattern, '')
         }
     }
 
     return (
         <div className="max-w-sm w-full lg:min-w-full lg:flex">
-            <div className="h-48 lg:h-auto lg:w-48 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center bg-gray-800 text-gray-200 pt-3">
+            <div className="lg:w-48 flex-none rounded-t lg:rounded-t-none lg:rounded-l text-center bg-gray-800 text-gray-200 pt-4 pb-8">
+                <div className="border-l-8 border-white border text-gray-200 p-1 mx-6 mt-1 rounded" role="alert">
+                    <p className="font-bold">{getFormattedDate().displayDay}</p>
+                </div>
                 <span className="text-6xl">{getFormattedDate().numericDay}<span className="text-3xl"> {getFormattedDate().month}</span></span>
                 <span className="block text-2xl -mt-4">{Time}</span>
+                
             </div>
             <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal w-full">
                 <div className="mb-8">

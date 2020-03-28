224af37d4e6a087dc31e05c284632de83b8bcbc3
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-03-28 20:45:14 +0800

    format code

--- src/components/Home/single-playlist.tsx
+++ src/components/Home/single-playlist.tsx
@@ -8,10 +8,10 @@ interface Props {
 }
 
 const SinglePlaylist: React.FunctionComponent<Props> = ({data}) => {
-    const { title, thumbnailUrl, organisation, durationType, videos } = data
+    const { title, thumbnailUrl, organisation, durationType, videos, id } = data
     return (
-        <div className="rounded overflow-hidden shadow-lg hover:shadow-2xl bg-white align-center cursor-pointer" onClick={() => navigate('/watch')}>
-            <img className="w-full z-10" src={thumbnailUrl} alt="Sunset in the mountains" />
+        <div className="rounded overflow-hidden shadow-lg hover:shadow-2xl bg-white align-center cursor-pointer" onClick={() => navigate(`/watch/${id}/1`)}>
+            <img className="w-full z-10" src={thumbnailUrl} alt={title} />
             <div className="px-6 py-4">
                 <div className="font-bold text-xl leading-tight">{title}</div>
                 <p className="text-gray-600 text-base">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default () => (
         <div className="max-w-sm mx-auto">
             <ul className="flex">
                 <li className="flex-1 mr-4">
-                    <a className="text-center block border border-teal-500 rounded py-2 px-4 bg-teal-500 hover:bg-teal-500 text-white" href="#playlists">Recorded Playlists</a>
+                    <a className="text-center block border border-teal-500 rounded py-2 px-4 bg-teal-500 hover:bg-teal-500 text-white cursor-pointer" href="#playlists">Recorded Playlists</a>
                 </li>
                 <li className="flex-1">
-                    <a className="text-center block border border-teal-500 rounded hover:border-gray-200 text-teal-500 hover:bg-gray-200 py-2 px-4" onClick={() => navigate('/live')}>Upcoming Lives</a>
+                    <a className="text-center block border border-teal-500 rounded hover:border-gray-200 text-teal-500 hover:bg-gray-200 py-2 px-4 cursor-pointer" onClick={() => navigate('/live')}>Upcoming Lives</a>
                 </li>
             </ul>
         </div>

11bdaff302eb422ce5040b8f53403cadb1365698
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-03-29 01:09:11 +0800

    clean up styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,12 +9,12 @@ const Header = () => {
         <nav className="flex items-center justify-between flex-wrap p-6 bg-gray-800 w-full">
             <div className="flex items-center flex-shrink-0 mr-6 cursor-pointer" onClick={() => navigate('/')}>
                 <FaVideo size="2em" className="text-teal-500"/>
-                <span className="font-medium text-2xl tracking-tight ml-2 text-gray-300">FatihaTV</span>
+                <span className="font-medium text-2xl tracking-tight ml-2 text-gray-200">FatihaTV</span>
             </div>
             <div className="flex-grow"></div>
             <div className="block">
                 <div>
-                    <a href="#" className="inline-block text-sm px-4 py-3 leading-none border rounded border-gray-300 text-gray-300 hover:border-teal-500 hover:text-white hover:bg-teal-500 lg:mt-0 uppercase">Share this page<FaShare className="-mt-1 ml-2 inline align-middle"/></a>
+                    <a href="#" className="inline-block text-sm px-4 py-3 leading-none border rounded border-gray-200 text-gray-200 hover:border-teal-500 hover:text-white hover:bg-teal-500 lg:mt-0 uppercase">Share this page<FaShare className="-mt-1 ml-2 inline align-middle"/></a>
                 </div>
             </div>
          </nav>

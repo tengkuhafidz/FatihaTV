ab1ec67e23780cb9802176652e1763b3d70c5511
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-03-29 11:15:00 +0800

    add seo component and made app a pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,18 +2,35 @@ module.exports = {
   siteMetadata: {
     title: "FatihaTV",
     description: "Watch local Islamic content from the comfort of your home.",
+    author: "Hafidz & Fatiha",
     url: "https://www.fatihatv.com", // No trailing slash allowed!
     image: "/images/fatihatv.jpg", // Path to your image you placed in the 'static' folder
   },
   plugins: [
     'gatsby-plugin-typescript',
     'gatsby-plugin-postcss',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
       {
         resolve: `gatsby-plugin-google-gtag`,
         options: {
           trackingIds: [
             "UA-162017890-1",
           ],
+        },
+      },
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `FatihaTV`,
+          short_name: `FatihaTV`,
+          start_url: `/`,
+          background_color: `#F7FAFC`,
+          theme_color: `#38B2AC`,
+          // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+          // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+          display: `standalone`,
+          icon: `static/app-icon.png`, // This path is relative to the root of the site.
         },
       },
   ],

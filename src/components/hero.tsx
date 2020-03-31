af375a873c29169a96c2021348ba047691b6df61
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-03-31 19:17:56 +0800

    fix tracking styling

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => (
     <div className="max-w-xs md:max-w-2xl mx-auto md:text-center pt-8 py-16">
         <h1 className="font-sans md:font-light md:tracking-wider text-2xl md:text-5xl text-gray-800 leading-tight">
             Watch local Islamic content
-            <span className="font-normal text-teal-500"> from the comfort of your home.</span>
+            <span className="font-normal text-teal-500 tracking-normal"> from the comfort of your home.</span>
         </h1>
         <p className="text-xl text-gray-600 font-light mt-4">
             Our local asatizah have been hard at work in producing online contents to ensure that we can continue to seek religious knowledge in this uncertain time. We curate them here to support their efforts. 

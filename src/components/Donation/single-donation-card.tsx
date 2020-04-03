735727e34ace9283b29e96820e32cb395f5b9f08
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-04-03 23:18:06 +0800

    update to attribute TabungMasjidSG

--- src/components/Donation/single-donation-card.tsx
+++ src/components/Donation/single-donation-card.tsx
@@ -9,7 +9,7 @@ const SingleDonationCard: React.FC<Props> = ({ donationData }) => {
     const { organisation, paynow_uen } = donationData
 
     return (  
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white hover:scale-110 hover:shadow-xl transform hover:scale-110">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white hover:scale-110 hover:shadow-xl transform hover:scale-110 border-2">
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{organisation}</div>
                 <p className="text-gray-700 text-base">
--- src/pages/donation-listing.tsx
+++ src/pages/donation-listing.tsx
@@ -4,12 +4,22 @@ import DonationLists from '../components/Donation/donation-lists'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { OutboundLink } from 'gatsby-plugin-google-gtag'
 
 export default () => (
     <Layout>
         <SEO title="Donate to Our Mosques" />
-        <Hero title="Contribute to our Mosques" subtext="Donations received by the mosques have significantly reduced due to it's closure. We list their PayNow UEN here for you to conveniently donate to them directly." />
-        <p className="text-gray-600 text-center mt-8 font-light text-sm">Last Updated: 2nd April 2020</p>
+        <Hero title="Contribute to our Mosques" subtext="Donations received by the mosques have significantly reduced due to it's closure. Our friends from TabungMasjidSG have curated a list of their PayNow UEN for you to conveniently donate to them directly." />
+        <p className="text-gray-600 text-center mt-8 font-light text-sm">Last Updated: 2nd April 2020 &middot; 
+            Curated by:&nbsp;
+            <OutboundLink 
+                target="_blank" 
+                className="underline" 
+                href="https://tabungmasjidsg.com"
+            >
+                TabungMasjidSG
+            </OutboundLink>
+        </p>
         <DonationLists />
         <BottomCta />
     </Layout>

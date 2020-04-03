876b6295e19ebe5f983e8b8533ceb802e16803a1
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-04-03 21:34:14 +0800

    update hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,10 @@ import SEO from '../components/seo'
 export default () => (
     <Layout>
         <SEO title="Local Islamic Videos" />
-        <Hero />
+        <Hero 
+            title="Watch local Islamic content"
+            subtext="Our local asatizah have been hard at work in producing online contents to ensure that we can continue to seek religious knowledge in this uncertain time. We curate them here to support their efforts."
+        />
         <div className="max-w-xs md:max-w-sm mx-auto">
             <ul className="flex">
                 <li className="flex-1 mr-4">

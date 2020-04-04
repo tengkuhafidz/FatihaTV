d7e72a97dfce0607c41046ff9bb283f0c8b46133
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-04-04 09:36:25 +0800

    fix copy

--- src/pages/donation-listing.tsx
+++ src/pages/donation-listing.tsx
@@ -9,7 +9,7 @@ import { OutboundLink } from 'gatsby-plugin-google-gtag'
 export default () => (
     <Layout>
         <SEO title="Donate to Our Mosques" />
-        <Hero title="Contribute to our Mosques" subtext="Donations received by the mosques have significantly reduced due to it's closure. Our friends from TabungMasjidSG have curated a list of their PayNow UEN for you to conveniently donate to them directly." />
+        <Hero title="Contribute to our Mosques" subtext="Donations received by the mosques have significantly reduced due to it's closure. Our friends from TabungMasjidSG have curated a list of their payment information for you to conveniently donate to them directly." />
         <p className="text-gray-600 text-center mt-8 font-light text-sm">Last Updated: 4th April 2020 &middot; 
             Curated by:&nbsp;
             <OutboundLink 

efd53e403d82c2851c2a4e06c0f45f5009701e56
Faruq Rasid <me@ruqqq.sg>
2020-04-11 12:42:08 +0800

    Initial Cypress setup

---
+++ cypress/integration/search_spec.js
@@ -0,0 +1,12 @@
+describe("Given user is on homepage, When user types into the search box,", () => {
+  it("The playlist list will return filtered result based on the words entered", () => {
+    cy.visit(Cypress.env("baseUrl"));
+    cy.get('[data-cy="search-box"]')
+      .focus()
+      .type("Fatris", { force: true });
+
+    cy.get('[data-cy="playlist-card"]')
+      .first()
+      .should("contain", "Kuliah Tafsir by Ustaz Fatris");
+  });
+});
--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -9,6 +9,7 @@ const SearchInput: React.FC<Props> = ({ handleSearchFilter }) => (
   <input
     className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-teal-500 mb-8"
     id="search"
+    data-cy="search-box"
     type="text"
     placeholder="Try this shiny new search feature! ^_^"
     onChange={(e): void => handleSearchFilter(e)}

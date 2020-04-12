b814f3b2c2673b8a109f07a57a86ad4f9e3c37ed
Abdul Aziz <me@ziz.ninja>
2020-04-12 11:53:12 +0800

    Allow using only one key for sheets and youtube

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,9 @@ module.exports = {
       resolve: "gatsby-source-data",
       options: {
         youtubeApiKey: process.env.GATSBY_YOUTUBE_API_KEY,
-        sheetsApiKey: process.env.GATSBY_SHEETS_API_KEY,
+        sheetsApiKey: process.env.GATSBY_SHEETS_API_KEY
+          ? process.env.GATSBY_SHEETS_API_KEY
+          : process.env.GATSBY_YOUTUBE_API_KEY,
         useLocal: process.env.GATSBY_USE_LOCAL_DATA || false,
       },
     },

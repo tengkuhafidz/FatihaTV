5511325669ea5fb3ab991549e67046f5115874f1
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-04-12 20:52:55 +0800

    fix searchbar styling

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 const SearchInput: React.FC<Props> = ({ handleSearchFilter }) => (
-  <div className="md:max-w-2xl mx-auto">
+  <div className="md:max-w-2xl mx-auto pr-8">
     <input
       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-teal-500 mb-8"
       id="search"

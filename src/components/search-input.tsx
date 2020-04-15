b9d3c2aa201be46d7af3a03591623f749730f6d9
Faruq Rasid <me@ruqqq.sg>
2020-04-15 09:39:07 +0800

    #18 Refactor

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -21,8 +21,8 @@ interface Props {
 
 const SearchInput: React.FC<Props> = ({
   handleSearchFilter,
-  selectedLanguage = "all",
   showLanguageSelector = false,
+  selectedLanguage = "all",
   handleLanguageSelected,
 }) => (
   <div className="md:max-w-2xl mx-auto pr-8 md:flex md:flex-row mb-8 md:flex-wrap">
@@ -35,36 +35,53 @@ const SearchInput: React.FC<Props> = ({
       onChange={(e): void => handleSearchFilter(e)}
     />
     {showLanguageSelector ? (
-      <div className="language-dropdown md:inline-block relative">
-        <button className="flex bg-gray-300 text-gray-700 py-2 px-3 rounded md:rounded-l-none shadow appearance-none inline-flex items-center h-10 iphone:w-full">
-          <span className="mr-1 font-semibold">Language: </span>
-          <span>{Languages[selectedLanguage]}</span>
-          <span className="flex-grow">&nbsp;</span>
-          <FaAngleDown size={16} className="ml-1" />
-        </button>
-        <ul
-          className="language-dropdown-menu absolute hidden text-gray-700 pt-1 w-full"
-          style={{ zIndex: 1001 }}
-        >
-          {Object.keys(Languages).map(code => (
-            <li key={code}>
-              <button
-                className="flex justify-between bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-full"
-                onClick={(): void =>
-                  handleLanguageSelected &&
-                  handleLanguageSelected(code as LanguageCode)
-                }
-              >
-                {Languages[code as LanguageCode]}
-                {selectedLanguage === code ? (
-                  <FaCheck size={14} className="mt-1" />
-                ) : null}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <LanguageSelector
+        selectedLanguage={selectedLanguage}
+        handleLanguageSelected={handleLanguageSelected}
+        className="md:inline-block"
+      />
     ) : null}
+  </div>
+);
+
+interface LanguageSelectorProps
+  extends Pick<Props, "selectedLanguage" | "handleLanguageSelected"> {
+  className?: string;
+}
+
+const LanguageSelector: React.FC<LanguageSelectorProps> = ({
+  selectedLanguage = "all",
+  handleLanguageSelected,
+  className = "",
+}) => (
+  <div className={`language-dropdown relative ${className}`}>
+    <button className="flex bg-gray-300 text-gray-700 py-2 px-3 rounded md:rounded-l-none shadow appearance-none inline-flex items-center h-10 iphone:w-full">
+      <span className="mr-1 font-semibold">Language: </span>
+      <span>{Languages[selectedLanguage]}</span>
+      <span className="flex-grow">&nbsp;</span>
+      <FaAngleDown size={16} className="ml-1" />
+    </button>
+    <ul
+      className="language-dropdown-menu absolute hidden text-gray-700 pt-1 w-full"
+      style={{ zIndex: 1001 }}
+    >
+      {Object.keys(Languages).map(code => (
+        <li key={code}>
+          <button
+            className="flex justify-between bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-full"
+            onClick={(): void =>
+              handleLanguageSelected &&
+              handleLanguageSelected(code as LanguageCode)
+            }
+          >
+            {Languages[code as LanguageCode]}
+            {selectedLanguage === code ? (
+              <FaCheck size={14} className="mt-1" />
+            ) : null}
+          </button>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 

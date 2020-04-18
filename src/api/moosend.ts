8d96c724445a1db808312c1a66eebe3fdb7ed727
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-04-18 14:30:47 +0800

    fix moosend api

--- src/api/moosend.ts
+++ src/api/moosend.ts
@@ -4,14 +4,13 @@ export const addSubscriber = async (email: string): Promise<boolean> => {
   const apiKey = process.env.GATSBY_MOOSEND_API_KEY;
   const mailingListId = process.env.GATSBY_MOOSEND_LIST_ID;
 
-  const url = `https://api.moosend.com/v3/subscribers/${mailingListId}/json?apikey=${apiKey}`;
+  const url = `https://api.moosend.com/v3/subscribers/${mailingListId}/subscribe.json?apikey=${apiKey}`;
   const request = {
     Email: email,
     CustomFields: {
       source: "website",
     },
   };
-  const { code } = await axios.post(url, request);
-
-  return code === 0 ? true : false;
+  const resp = await axios.post(url, request);
+  return resp.status === 200 ? true : false;
 };

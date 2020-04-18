a5ee48bbcb6e93298f0717150baaccb20c0dafe7
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-04-19 01:04:18 +0800

    fix thumbail size issue for mobile and standardize capitalisation for playlist titles

--- src/components/Home/single-playlist.tsx
+++ src/components/Home/single-playlist.tsx
@@ -24,13 +24,17 @@ const SinglePlaylist: React.FC<Props> = ({ playlist, videoId }) => {
   return (
     <div
       data-cy="playlist-card"
-      className={`overflow-hidden align-center cursor-pointer inline-block pr-4 sm:w-48 md:w-full`}
+      className={`overflow-hidden align-center cursor-pointer inline-block pr-4 md:w-full`}
       onClick={handleClick}
     >
-      <img className="w-full z-10" src={thumbnailUrl} alt={title} />
+      <img className="w-48 md:w-full z-10" src={thumbnailUrl} alt={title} />
       <div>
-        <div className="font-semibold leading-tight truncate">{title}</div>
-        <p className="text-gray-600 text-sm truncate">{organisationName}</p>
+        <div className="font-semibold w-48 md:w-full leading-tight truncate capitalize">
+          {title.toLowerCase()}
+        </div>
+        <p className="text-gray-600 text-sm truncate capitalize">
+          {organisationName}
+        </p>
         <p className="text-gray-600 text-sm truncate">
           {publishedAt} &middot; {videos.length}{" "}
           {videos.length === 1 ? "video " : "videos"}

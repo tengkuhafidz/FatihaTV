935037abcaa4cb94a78dcb57127b0430eb2d318d
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-04-19 01:08:10 +0800

    change dropdown bg color and added shadowfor language filter to accomodate new main bg color

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -82,7 +82,7 @@ const LanguageSelector: React.FC<LanguageSelectorProps> = ({
           <li key={code}>
             <button
               data-cy="language-selector-choice"
-              className="flex justify-between bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-full"
+              className="flex justify-between bg-gray-100 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap w-full shadow"
               onClick={(): void => {
                 setMenuVisible(false);
                 handleLanguageSelected?.(code as LanguageCode);

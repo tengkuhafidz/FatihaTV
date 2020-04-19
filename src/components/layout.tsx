54cb1a72642118e1ad78a83ea913f1483590c439
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-04-20 02:51:48 +0800

    add birdseed

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,7 @@ import DonationBanner from "./banner";
 import Footer from "./footer";
 import Header from "./header";
 import BackToTop from "./back-to-top";
+import Helmet from "react-helmet";
 
 enableSmoothScroll();
 
@@ -19,6 +20,18 @@ const Layout: React.FC<Props> = ({ children, showBanner = false }) => (
     <main>{children}</main>
     <Footer />
     <BackToTop />
+    <Helmet>
+      <script
+        defer
+        id="io.birdseed.script-tag"
+        type="text/javascript"
+        src="https://cdn.birdseed.io/widget.js"
+      ></script>
+      <div
+        id="birdseed-widget-container"
+        data-token="64c3d69ae1d63222e211a82aefc2ea8f"
+      ></div>
+    </Helmet>
   </div>
 );
 

633d9d169ebe3929d66d22c1aeb27ea337bcdc57
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-04-23 14:36:06 +0800

    fix seo copy

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,8 @@ module.exports = {
   siteMetadata: {
     title: "FatihaTV",
     titleTemplate: "%s | FatihaTV",
-    description: "Watch local Islamic contents from the comfort of your home.",
+    description:
+      "Watch beneficial content this Ramadan without other distractions.",
     author: "Hafidz & Fatiha",
     url: "https://fatihatv.com", // No trailing slash allowed!
     image: "/share-banner.jpg" // Path to your image you placed in the 'static' folder

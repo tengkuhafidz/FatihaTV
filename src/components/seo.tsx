26ed9d9353364b6d7bb4ddf17aa0a17da3829b4b
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-05-02 09:59:05 +0800

    change card style for twitter

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -69,7 +69,7 @@ const SEO: React.FC<Props> = ({
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
